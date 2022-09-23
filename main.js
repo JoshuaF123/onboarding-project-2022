0f658d4de05c44ec76a8c1873cf0a9a3756c06d6
Joshua Fay <joshua.fay@exelaration.com>
2022-09-23 16:07:54 -0400

    Got the country menu to give a list of countries

--- main.js
+++ main.js
@@ -3,15 +3,15 @@ console.log("hello");
 async function readCountryData(){
     try{
         console.log(document);
-        var element = document.getElementById("countyList");
         let fetchRes = await fetch("https://xc-countries-api.herokuapp.com/api/countries/");
         let res = await fetchRes.json();
-        var countries = [];
+        let countries = [];
 
         res.forEach(element => {
             countries.push(element)
         });
 
+        //console.log(countries);
         return countries;
 
     }catch(err){
@@ -19,9 +19,18 @@ async function readCountryData(){
     }
 }
 
-function populateDropdownMenu(){
-    countries = readCountryData();
-    
+async function populateDropdownMenu(){
+    let countries = await readCountryData();
+    console.log(countries);
+    let dropdownMenu = document.getElementById("countryList");
+    for(let x = 0 ; x < countries.length; x++){
+        var country = countries[x];
+        var el = document.createElement("option");
+        el.text = country.name;
+        el.value = country.code;
+        dropdownMenu.add(el);
+    }
 }
 
+window.addEventListener('DOMContentLoaded', readCountryData);
 window.addEventListener('DOMContentLoaded', populateDropdownMenu);
