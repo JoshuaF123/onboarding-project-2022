c53149b333718cd870d6d0c1e8fbbc86be8249ff
Joshua Fay <joshua.fay@exelaration.com>
2022-09-27 16:30:09 -0400

    Finally! User can select different states and the state list wwill update every time with no duplicates in either dropdownmenus. Time for step 3.

--- main.js
+++ main.js
@@ -1,14 +1,13 @@
 console.log("hello");
 
 /*
-This function fetchs the country list from the provided URL,
+This function fetchs the list from the provided URL,
 saves it in a JSON array, then puts that data into a useable
-array. Try to rework so that it works for state and country.
+array.
 @return: A usable array containing all the data from the URL.
 */    
 async function readDatabase(url){
     try{
-        //let tempURL = encodeURI(url)
         let fetchRes = await fetch(url.toString());
         let res = await fetchRes.json();
         let dataStorage = [];
@@ -26,7 +25,7 @@ async function readDatabase(url){
 
 /*
 The functions takes the information that populateDropdownMenu
-collects, then adds it to the country dropdown menu.
+collects, then adds it to the appropriate dropdown menu.
 */
 async function populateDropdownMenu(id, url){
     let optionsFromDatabase = await readDatabase(url);
@@ -48,27 +47,33 @@ async function populateDropdownMenu(id, url){
     }
 }
 
-window.addEventListener('DOMContentLoaded', function(){
+window.addEventListener('DOMContentLoaded', function(){//end1
     let clickedCountry = false;
-    let clickedState = false
-    document.getElementById("countryList").addEventListener('click', function(){
+    let countryList = document.getElementById("countryList");
+    let stateList = document.getElementById("stateList");
+
+    countryList.addEventListener('click', function(){ //start2
         if(clickedCountry === false){
             populateDropdownMenu("countryList", "https://xc-countries-api.herokuapp.com/api/countries/");
             clickedCountry = true;
         }
-    });
-    document.getElementById("stateList").addEventListener('click', function(){
-        if(clickedState === false){
-            let urlPt1 = "https://xc-countries-api.herokuapp.com/api/countries/";
-            let urlPt2 = "/states/";
-    
-            let choice = document.getElementById("countryList");    
-            let choosenCountry = choice.value;
-    
-            let finalURL = urlPt1+choosenCountry+urlPt2;
-            populateDropdownMenu("stateList", finalURL);
-            clickedState = true;
-        }
+        
+    });//end2
 
-    });
-});
+    countryList.addEventListener('change', function(){ //start3
+        let urlPt1 = "https://xc-countries-api.herokuapp.com/api/countries/";
+        let urlPt2 = "/states/";
+        let choosenCountry = countryList.value;
+        let finalURL = urlPt1+choosenCountry+urlPt2;
+
+        var options = document.querySelectorAll("#stateList option");
+        options.forEach(o => o.remove());
+        
+        let defOption = document.createElement("option");
+        defOption.text = "Select a state.";
+        defOption.val = "val";
+        stateList.add(defOption);
+
+        populateDropdownMenu("stateList", finalURL);
+    });//start3
+});//end1
