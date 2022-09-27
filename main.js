d4d79270e4051b1d86ec6ed90743e721d9ef46a8
Joshua Fay <joshua.fay@exelaration.com>
2022-09-27 17:51:59 -0400

    Began working on the add country functionality

--- main.js
+++ main.js
@@ -4,6 +4,8 @@ console.log("hello");
 This function fetchs the list from the provided URL,
 saves it in a JSON array, then puts that data into a useable
 array.
+@param url: is the url of the database you're trying to pull
+from. It must be a string!
 @return: A usable array containing all the data from the URL.
 */    
 async function readDatabase(url){
@@ -26,18 +28,16 @@ async function readDatabase(url){
 /*
 The functions takes the information that populateDropdownMenu
 collects, then adds it to the appropriate dropdown menu.
+@param id: the ID of the dropdown list you're trying to access.
+Must be a string.
+@param url: is the url of the database you're trying to pull
+from. It must be a string!
 */
 async function populateDropdownMenu(id, url){
     let optionsFromDatabase = await readDatabase(url);
     console.log(optionsFromDatabase);
     let dropdownMenu = document.getElementById(id);
-    //This clears out the options everytime you click the
-    //dropdown menu, but that prevents us from having a
-    //selected element
-    /*dropdownMenu.onclick = function(){
-        var options = document.querySelectorAll("#"+ id +" option");
-        options.forEach(o => o.remove());
-    }*/
+
     for(let x = 0 ; x < optionsFromDatabase.length; x++){
         var place = optionsFromDatabase[x];
         var el = document.createElement("option");
@@ -47,11 +47,21 @@ async function populateDropdownMenu(id, url){
     }
 }
 
+async function addCountry(url){
+    const response = await fetch(url, {method: 'POST'});
+}
+
+//This part is basically the main function. On load, populate the
+//country dropdown menu by calling 'populateDropdownMenu' function.
+//This in turn forces the state list to populate. The statelist will update
+//whenever the user changes the currently selected country.
 window.addEventListener('DOMContentLoaded', function(){//end1
     let clickedCountry = false;
     let countryList = document.getElementById("countryList");
     let stateList = document.getElementById("stateList");
 
+    //Initial population of the country list. The If statement
+    //safeguards from duplications or pollution.
     countryList.addEventListener('click', function(){ //start2
         if(clickedCountry === false){
             populateDropdownMenu("countryList", "https://xc-countries-api.herokuapp.com/api/countries/");
@@ -59,16 +69,19 @@ window.addEventListener('DOMContentLoaded', function(){//end1
         }
         
     });//end2
-
+    //auto-updating state list.
     countryList.addEventListener('change', function(){ //start3
         let urlPt1 = "https://xc-countries-api.herokuapp.com/api/countries/";
         let urlPt2 = "/states/";
         let choosenCountry = countryList.value;
         let finalURL = urlPt1+choosenCountry+urlPt2;
 
+        //These two lines remove every entry in the state list
+        //to prevent duplications and polluting the options.
         var options = document.querySelectorAll("#stateList option");
         options.forEach(o => o.remove());
         
+        //This block re-adds the default value to the state list.
         let defOption = document.createElement("option");
         defOption.text = "Select a state.";
         defOption.val = "val";
