a201a3fc61cf70f31e45e5a2c447fe1c0d914c81
Joshua Fay <joshua.fay@exelaration.com>
2022-09-27 13:58:14 -0400

    Made the existing code reuseable. Need to solve the add-elements-to-database-repeatedly problem

--- main.js
+++ main.js
@@ -6,19 +6,19 @@ saves it in a JSON array, then puts that data into a useable
 array. Try to rework so that it works for state and country.
 @return: A usable array containing all the data from the URL.
 */    
-async function readCountryData(){
+async function readDatabase(url){
     try{
-        console.log(document);
-        let fetchRes = await fetch("https://xc-countries-api.herokuapp.com/api/countries/");
+        console.log(url);
+        //let tempURL = encodeURI(url)
+        let fetchRes = await fetch(url.toString());
         let res = await fetchRes.json();
-        let countries = [];
+        let dataStorage = [];
 
         res.forEach(element => {
-            countries.push(element)
+            dataStorage.push(element)
         });
 
-        //console.log(countries);
-        return countries;
+        return dataStorage;
 
     }catch(err){
         console.log(err);
@@ -29,29 +29,35 @@ async function readCountryData(){
 The functions takes the information that populateDropdownMenu
 collects, then adds it to the country dropdown menu.
 */
-async function populateDropdownMenu(){
-    let countries = await readCountryData();
-    console.log(countries);
-    let dropdownMenu = document.getElementById("countryList");
-    for(let x = 0 ; x < countries.length; x++){
-        var country = countries[x];
+async function populateDropdownMenu(id, url){
+    let optionsFromDatabase = await readDatabase(url);
+    console.log(optionsFromDatabase);
+    let dropdownMenu = document.getElementById(id);
+    dropdownMenu.onclick = function(){
+        var options = document.querySelectorAll("#countryList option");
+        options.forEach(o => o.remove());
+    }
+    for(let x = 0 ; x < optionsFromDatabase.length; x++){
+        var place = optionsFromDatabase[x];
         var el = document.createElement("option");
-        el.text = country.name;
-        el.id = country.code;
+        el.text = place.name;
+        el.value = place.code;
         dropdownMenu.add(el);
     }
 }
 
 window.addEventListener('DOMContentLoaded', function(){
     document.getElementById("countryList").addEventListener('click', function(){
-        readCountryData();
-        populateDropdownMenu();
+        populateDropdownMenu("countryList", "https://xc-countries-api.herokuapp.com/api/countries/");
     })
 });
 
 window.addEventListener('DOMContentLoaded', function(){
     document.getElementById("stateList").addEventListener('click', function(){
-        readCountryData();
-        populateDropdownMenu();
+        let urlPt1 = "https://xc-countries-api.herokuapp.com/api/countries/";
+        let choosenCountry = "";
+        let urlPt2 = "/states/";
+        let finalURL = urlPt1+choosenCountry+urlPt2;
+        populateDropdownMenu("stateList");
     })
 });
